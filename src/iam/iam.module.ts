862270d28a1e2a87b32f4e0524de02dc96b7ee04
Diego Vicuna <diego.vicuna@pop-os.localdomain>
2024-06-03 11:46:09 -0500

    feature: passport for session auth
    - redis added

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthenticationController } from './authentication/authentication.controller';
@@ -23,39 +23,71 @@ import {ApiKeyGuard} from "./authentication/guards/api-key/api-key.guard";
 import { GoogleAuthenticationService } from './authentication/social/google-authentication.service';
 import { GoogleAuthenticationController } from './authentication/social/google-authentication.controller';
 import { OtpAuthenticationService } from './authentication/otp-authentication.service';
+import { SessionAuthenticationService } from './authentication/session-authentication.service';
+import { SessionAuthenticationController } from './authentication/session-authentication.controller';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { UserSerializer } from './authentication/serializers/user-serializer/user-serializer';
+import * as createRedisStore from 'connect-redis';
+import connectRedis, {RedisStore} from 'connect-redis';
+import Redis from 'ioredis';
 
 @Module({
-  imports:[
-    TypeOrmModule.forFeature([
-        User,
-        ApiKey
-    ]),
-    JwtModule.registerAsync(jwtConfig.asProvider()),
-    ConfigModule.forFeature(jwtConfig)
-  ],
-  providers: [
-    {
-    provide: HashingService,
-    useClass: BcryptService, // 👈
-    },
-    AuthenticationService,
-    {
-      provide: APP_GUARD, 
-      useClass: AuthenticationGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: PoliciesGuard //PermissionsGuard //RolesGuard
-    },
-    AccessTokenGuard,
-    ApiKeyGuard,
-    RefreshTokenIdsStorage,
-    PolicyHandlerStorage,
-    FrameworkContributorPolicyHandler,
-    ApiKeysService,
-    GoogleAuthenticationService,
-    OtpAuthenticationService
-  ],
-  controllers: [AuthenticationController, GoogleAuthenticationController]
+    imports: [
+        TypeOrmModule.forFeature([User, ApiKey]),
+        JwtModule.registerAsync(jwtConfig.asProvider()),
+        ConfigModule.forFeature(jwtConfig),
+    ],
+    providers: [
+        {
+            provide: HashingService,
+            useClass: BcryptService,
+        },
+        {
+            provide: APP_GUARD,
+            useClass: AuthenticationGuard,
+        },
+        {
+            provide: APP_GUARD,
+            useClass: PoliciesGuard, // RolesGuard,
+        },
+        AccessTokenGuard,
+        ApiKeyGuard,
+        RefreshTokenIdsStorage,
+        AuthenticationService,
+        PolicyHandlerStorage,
+        FrameworkContributorPolicyHandler,
+        ApiKeysService,
+        OtpAuthenticationService,
+        GoogleAuthenticationService,
+        SessionAuthenticationService,
+        UserSerializer,
+    ],
+    controllers: [
+        AuthenticationController,
+        GoogleAuthenticationController,
+        SessionAuthenticationController,
+    ],
 })
-export class IamModule {}
+export class IamModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        const RedisStore = require("connect-redis").default;
+
+        consumer
+            .apply(
+                session({
+                    store: new RedisStore({ client: new Redis(6379, 'nest-auth-redis') }),
+                    secret: process.env.SESSION_SECRET,
+                    resave: false,
+                    saveUninitialized: false,
+                    cookie: {
+                        sameSite: true,
+                        httpOnly: true,
+                    },
+                }),
+                passport.initialize(),
+                passport.session(),
+            )
+            .forRoutes('*');
+    }
+}
